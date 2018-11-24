1697c4418d2b5332cfb89f23806088bd0034a882
reyx <soufiane.nass7@gmail.com>
2018-11-24 15:39:11 +0100

    Add apollo client module , and start configuring it #2

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Client from "./config/api";
+import { ApolloProvider } from "react-apollo";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = props => {
+	return (
+		<ApolloProvider client={Client}>
+			<App />>
+		</ApolloProvider>
+	);
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
