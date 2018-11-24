26750803a129f48b4a9623fdc8429f3d06f43712
reyx <soufiane.nass7@gmail.com>
2018-11-24 15:45:37 +0100

    Add auth middleware to the graphql client to use jwt token #2

--- frontend/config/api.js
+++ frontend/config/api.js
@@ -1,5 +1,20 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { ApolloLink, concat } from "apollo-link";
+
+const httpLink = new HttpLink({ uri: "/graphql" });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+	// add the authorization to the headers
+	operation.setContext({
+		headers: {
+			authorization: localStorage.getItem("token") || null
+		}
+	});
+
+	return forward(operation);
+});
 
 export default new ApolloClient({
-	uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+	link: concat(authMiddleware, httpLink)
 });
