0c1c6e0cd65149a627f97ffff8b67221c2945197
reyx <soufiane.nass7@gmail.com>
2018-11-24 05:40:26 +0100

    Start new django app user to manage accounts logic #1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+AUTH_USER_MODEL = 'user.User'
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webpack_loader',
     'graphene_django',
+    'backend.user'
 ]
 
 MIDDLEWARE = [
