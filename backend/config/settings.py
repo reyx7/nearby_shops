6239c7ce788367440d02f3f162b78f6f3b16192e
reyx <soufiane.nass7@gmail.com>
2018-11-24 03:07:57 +0100

    Turn off debug mode in master branch

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(BASE_DIR)
 SECRET_KEY = '^l0)kseqhzbvj7pn$p$$#ov$q%@qb&7oc$*ruiimr(z-f*6u8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
