6474b2ee326047783c6fb678cd6309171482b48b
reyx <soufiane.nass7@gmail.com>
2018-11-24 04:17:43 +0100

    Configure jwt auth and create mutation to manage token #1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 
@@ -146,5 +152,8 @@ WEBPACK_LOADER = {
 }
 
 GRAPHENE = {
-    'SCHEMA': 'backend.config.schema.schema'
+    'SCHEMA': 'backend.config.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
