840ce5cacf976680ea46a6fbfc80cc2d9da50174
reyx <soufiane.nass7@gmail.com>
2018-11-28 23:14:02 +0100

    Refactore file forntend/container/shops.jsx #9

--- frontend/container/Shops.jsx
+++ frontend/container/Shops.jsx
@@ -53,95 +53,99 @@ const REMOVE_SHOP = gql`
 	}
 `;
 
+const FavoriteShops = () => (
+	<Grid container spacing={16}>
+		<Query query={GET_FAVORITE_SHOPS}>
+			{({ data, loading, error }) => {
+				if (loading) return null;
+				if (error) return null;
+				return data.preferredShops.map(
+					({ id, name, picture, country, city }) => (
+						<Mutation mutation={REMOVE_SHOP}>
+							{remove => (
+								<Shop
+									key={id}
+									name={name}
+									url={picture}
+									country={country}
+									city={city}
+									handleRemove={() => {
+										remove({
+											variables: {
+												pk: id
+											}
+										});
+										document.location.reload();
+									}}
+								/>
+							)}
+						</Mutation>
+					)
+				);
+			}}
+		</Query>
+	</Grid>
+);
+
+const NearbyShops = () => (
+	<Grid container spacing={16}>
+		<Query query={GET_NEARBY_SHOPS}>
+			{({ data, loading, error }) => {
+				if (loading) return null;
+				if (error) return null;
+				return (
+					<Mutation mutation={LIKE_SHOP}>
+						{like => (
+							<Mutation mutation={DISLIKE_SHOP}>
+								{dislike =>
+									data.nearbyShops.map(
+										({
+											id,
+											name,
+											picture,
+											country,
+											city
+										}) => (
+											<Shop
+												key={id}
+												name={name}
+												url={picture}
+												country={country}
+												city={city}
+												handleLike={() => {
+													like({
+														variables: {
+															pk: id
+														}
+													});
+													document.location.reload();
+												}}
+												handleDislike={() => {
+													dislike({
+														variables: {
+															pk: id
+														}
+													});
+													document.location.reload();
+												}}
+											/>
+										)
+									)
+								}
+							</Mutation>
+						)}
+					</Mutation>
+				);
+			}}
+		</Query>
+	</Grid>
+);
+
 const Shops = ({ location, match }) => {
 	if (location.pathname === `${match.url}/favorite_shops`)
-		return (
-			<Grid container spacing={16}>
-				<Query query={GET_FAVORITE_SHOPS}>
-					{({ data, loading, error }) => {
-						if (loading) return null;
-						if (error) return null;
-						return data.preferredShops.map(
-							({ id, name, picture, country, city }) => (
-								<Mutation mutation={REMOVE_SHOP}>
-									{remove => (
-										<Shop
-											key={id}
-											name={name}
-											url={picture}
-											country={country}
-											city={city}
-											handleRemove={() => {
-												remove({
-													variables: {
-														pk: id
-													}
-												});
-												document.location.reload();
-											}}
-										/>
-									)}
-								</Mutation>
-							)
-						);
-					}}
-				</Query>
-			</Grid>
-		);
+		return <FavoriteShops />;
 	if (location.pathname === `${match.url}/nearby_shops`)
-		return (
-			<Grid container spacing={16}>
-				<Query query={GET_NEARBY_SHOPS}>
-					{({ data, loading, error }) => {
-						if (loading) return null;
-						if (error) return null;
-						return (
-							<Mutation mutation={LIKE_SHOP}>
-								{like => (
-									<Mutation mutation={DISLIKE_SHOP}>
-										{dislike =>
-											data.nearbyShops.map(
-												({
-													id,
-													name,
-													picture,
-													country,
-													city
-												}) => (
-													<Shop
-														key={id}
-														name={name}
-														url={picture}
-														country={country}
-														city={city}
-														handleLike={() => {
-															like({
-																variables: {
-																	pk: id
-																}
-															});
-															document.location.reload();
-														}}
-														handleDislike={() => {
-															dislike({
-																variables: {
-																	pk: id
-																}
-															});
-															document.location.reload();
-														}}
-													/>
-												)
-											)
-										}
-									</Mutation>
-								)}
-							</Mutation>
-						);
-					}}
-				</Query>
-			</Grid>
-		);
+		return <NearbyShops />;
 	return <div />;
 };
 
